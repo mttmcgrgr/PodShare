2dd5035b1f060d34609d66d693bb16fe5fd43610
Matt McGregor <mttmcgrgr@gmail.com>
2017-08-31 00:56:36 -0700

    setup user and session controllers

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,6 @@
 class Api::UsersController < ApplicationController
+
+  def create
+    
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,42 @@
 class User < ApplicationRecord
+
+  validates :session_token, :username, :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  ## adding allow_nil: true allows validation to pass when password instance variable is nil.
+  ## password instance variable only needs to exist when setting or changing password
+
+  before_validation :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user
+    user.password_is?(password) ? user : nil
+  end
+
+  def password_is?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    ##sets password input to instance variable but does NOT save to db, just for validation purposes
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def new_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token
+    self.session_token = new_session_token
+    self.save
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||=new_session_token
+  end
+
 end
